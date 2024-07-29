3d0f941e6eacff976ac6c88ecb23c187269cb402
Soudeep shaw <119213922+Soudeepshaw@users.noreply.github.com>
2024-07-29 21:32:44 +0530

    Update index.ts

--- backend1/src/index.ts
+++ backend1/src/index.ts
@@ -1,20 +1,38 @@
 import { WebSocketServer } from "ws";
 import { GameManager } from "./Gamemanager";
+import * as https from 'https';
+import express from 'express';
+import cors from 'cors';
 
-const port = 8080;
-const ws=new WebSocketServer({port});
-const gameManager=new GameManager();
+const port =8080;
+
+const app = express();
+const server = https.createServer(app);
+const ws = new WebSocketServer({ server });
+
+const corsOptions = {
+  origin: 'https://chess-with-react-frontend.onrender.com',
+  methods: ['GET', 'POST']
+};
+
+app.use(cors(corsOptions));
+
+const gameManager = new GameManager();
+
 ws.on('connection', function connection(ws) {
     console.log("New client connected");
     gameManager.addUser(ws);
-    ws.on("close",()=>{
-        console.log("Client disconnected"); 
+
+    ws.on("close", () => {
+        console.log("Client disconnected");
         const game = gameManager.getGameByPlayer(ws);
         if (game) {
             game.handlePlayerLeft(ws);
         }
-        gameManager.removeUser(ws)})
-
-    
+        gameManager.removeUser(ws);
+    });
 });
-console.log(`WebSocket server is running on ws://localhost:${port}/`);
+
+server.listen(port, () => {
+    console.log(`WebSocket server is running on wss://0.0.0.0:${port}/`);
+});
